5d2eb01ff8db9ac64fcd53769ad10eb82d95aaba
kei <kei.khcheung@gmail.com>
2021-05-24 16:04:58 +0200

    Nav bar + footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,66 @@
+const fontFamily = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sign': '"Space Grotesk", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      },
+      // Change headings font to as defined by `sign`
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              fontFamily: `${theme('fontFamily.sign')}`,
+              fontWeight: "900",
+              letterSpacing: "-0.02em"
+            },
+            h2: {
+              fontFamily: `${theme('fontFamily.sign')}`,
+            },
+            h3: {
+              fontFamily: `${theme('fontFamily.sign')}`,
+            },
+            h4: {
+              fontFamily: `${theme('fontFamily.sign')}`,
+            }
+          }
+        }
+      })
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    // Add `before:content` & `after:content` pseudo classes for ::before and ::after
+    // Working with data-content pseudo classes
+    plugin(({ addVariant, e }) => {
+      addVariant('before', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`before${separator}${className}`)}::before`;
+        });
+      });
+      addVariant('after', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`after${separator}${className}`)}::after`;
+        });
+      });
+    }),
+    plugin(({ addUtilities }) => {
+      const contentUtilities = {
+        '.content': {
+          content: 'attr(data-content)',
+        },
+        '.content-before': {
+          content: 'attr(data-before)',
+        },
+        '.content-after': {
+          content: 'attr(data-after)',
+        },
+      };
+      addUtilities(contentUtilities, ['before', 'after']);
+    }),
+  ],
 };
