6bcbb1d37dd7e351bff1c2f360d7c2cf29bdc5b8
kei <kei.khcheung@gmail.com>
2021-06-09 01:22:37 +0200

    Footnotes to sidenotes, wikilinks, backlinks. Post, page & note layouts.

--- js/sidenotes.js
+++ js/sidenotes.js
@@ -1,7 +1,5 @@
 const footnotes = document.querySelectorAll('.footnotes p');
-console.log(footnotes)
-const footnotes_anchors = document.querySelectorAll('sup');
-console.log(footnotes_anchors)
+const footnotes_anchors = document.querySelectorAll('[role="doc-noteref"]');
 
 function docReady(fn) {
   // See if document is fully loaded.
@@ -22,39 +20,12 @@ footnotes_anchors.forEach((element, index) => {
   // Remove unwanted trailing non-breaking space.
   let fn_text = fn.innerHTML;
   fn_text = fn_text.replace(/(&nbsp;)+$/g, '');
-
-  // Replace footnote anchor with sidenote.
+  // Create the DOM element for replacement.
   let sn_toggle = document.createElement('label');
   sn_toggle.setAttribute('for', index);
   sn_toggle.classList.add('margin-toggle', 'sidenote-number');
+  // Replace footnote anchor with sidenote.
   element.parentNode.replaceChild(sn_toggle, element);
+  // Add additional trailing DOM element to the replaced element.
   sn_toggle.insertAdjacentHTML('afterend', '<input type="checkbox" id="' + index + '" class="margin-toggle"/><span class="sidenote">' + fn_text + '</span>');
 });
-
-// docReady(function() {
-//   // Remove footnote backlink.
-//   let fn = footnotes[index];
-//   fn.lastElementChild.remove();
-//   // Remove unwanted trailing non-breaking space.
-//   let fn_sn = fn.innerHTML;
-//   fn_sn = fn_sn.replace(/(&nbsp;)+$/g, '');
-  
-//   // Replace footnote anchor with sidenote.
-//   let sidenote = document.createElement('label');
-//   sidenote.setAttribute('for', index);
-//   sidenote.classList.add('margin-toggle', 'sidenote-number');
-//   sidenote.innerHTML = '<input type="checkbox" id="' + index + '" class="margin-toggle"/><span class="sidenote">' + fn_sn + '</span>';
-//   footnotes_anchors[index].parentNode.replaceChild(sidenote, footnotes_anchors[index]);
-// });
-
-//   Array.prototype.forEach.call(footnotes_HTML, function(index){
-//     // Remove the return link and the preceding non-breaking space at the end of each footnote.
-//     var fn_back = footnotes[index].removeChild(footnotes[index].childNodes.lastChild);
-//     // Remove the extra non-breaking space in the sidenote text.
-//     var fn_sn = footnotes[index].innerHTML.replace(/(&nbsp;)+$/g, '');
-//     // Copy footnotes and place it in the side note margin.
-//     this.replaceWith("<label for='"+index+"' class='margin-toggle sidenote-number'></label><input type='checkbox' id='"+index+"' class='margin-toggle'/><span class='sidenote'>"+fn_sn+"</span>")
-//   }
-//   // Hide footnotes at the bottom of the page.
-//   // document.querySelector(".footnotes").hide();
-// )});
